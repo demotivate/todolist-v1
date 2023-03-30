e8b7bca2a5ca5647b8796609c8db8e8eb3e45cd8
Ethan Chen <ethansunchen@gmail.com>
2023-03-29 18:31:19 -0600

    268. Passing Data from Your Webpage to Your Server

--- app.js
+++ app.js
@@ -3,14 +3,27 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", (req, res) => {
-    var day = new Date();
+var items = ["buy food", "cook food", "eat food"];
 
-    if (day.getDay() === 0 || day.getDay() === 6) {
-        res.send("hooray");
-    } else {
-        res.sendFile(__dirname + "/index.html");
-    }
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    var date = new Date();
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+    };
+    var day = date.toLocaleDateString("en-US", options);
+
+    res.render("list", { day: day, newListItems: items });
+});
+
+app.post("/", (req, res) => {
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
 });
 
 app.listen("3000", function () {
