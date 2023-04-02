c9ccb6936115449e3e872d128061a4b00cf791f0
demotivate <50751626+demotivate@users.noreply.github.com>
2023-04-01 23:00:42 -0600

    271. Understanding Templates vs. Layouts

--- app.js
+++ app.js
@@ -1,30 +1,51 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { request } = require("express");
 
 const app = express();
 
-var items = ["buy food", "cook food", "eat food"];
+let items = ["buy food", "cook food", "eat food"];
+let workItems = [];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    var date = new Date();
-    var options = {
+    let date = new Date();
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long",
     };
-    var day = date.toLocaleDateString("en-US", options);
+    let day = date.toLocaleDateString("en-US", options);
 
-    res.render("list", { day: day, newListItems: items });
+    res.render("list", { listTitle: day, newListItems: items });
+});
+
+app.get("/work", function (req, res) {
+    res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.get("/about", function (req, res) {
+    res.render("about");
 });
 
 app.post("/", (req, res) => {
-    var item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+    let item = req.body.newItem;
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+
+        res.redirect("/");
+    }
+});
+
+app.post("/work", (req, res) => {
+    if (req.body.list === "work") workItems.push(item);
+    res.redirect("/work");
 });
 
 app.listen("3000", function () {
